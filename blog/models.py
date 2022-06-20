c1932d633947d785d8060cad48fe88a47e9f793e
Boss2021-eng <77675650+Boss2021-eng@users.noreply.github.com>
2022-06-20 09:53:37 +0100

    Update models.py

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,10 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 # Create your models here.
 class Post(models.Model):
     Title: models.CharField(max_length=200)
-    Text: models.TextField()
-    Author: models.CharField(max_length=50)
+    Text: models.TextField() 
+    Author: models.ForeignKey(get_user_model(),on_delete=models.CASCADE)
     created_date: models.DateTimeField()
     published_date: models.DateTimeField()
